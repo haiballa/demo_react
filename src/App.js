2f36cf5ab090ffd9cede192c8f996b93102d5101
cdufour <opusidea@gmail.com>
2022-11-21 09:53:39 +0000

    gitlab file

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Apprendre React
+          Se former à React 
         </a>
       </header>
     </div>
